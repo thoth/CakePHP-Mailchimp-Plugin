396224edee378c743a1185bcdb76143a34cb569f
Tom Rader <tom.rader@claritymediasoltions.com>
2013-12-02 09:42:22 -0700

    Fixes for send

--- Controller/Component/MailchimpComponent.php
+++ Controller/Component/MailchimpComponent.php
@@ -6,11 +6,11 @@
  * #date        08/09/12
  */
  
-App::import('Vendor', 'Mailchimp.MCAPI');
+App::import('Vendor', 'Mailchimp.MCAPI2');
 class MailchimpComponent extends Component {
 
 	
-	public function initialize($controller, $settings = NULL) {
+	public function initialize(Controller $controller, $settings = NULL) {
 		if ($controller->request->is('ajax')) {
 			Configure::write('debug', 0);
 
@@ -30,18 +30,19 @@ class MailchimpComponent extends Component {
 		}
 
 		$this->controller = $controller;
-		Configure::load('Mailchimp.config');
-		$this->api = new MCAPI(Configure::read('Mailchimp.apikey'));
+		
+		//$this->api = new MCAPI(Configure::read('Mailchimp.api_key'));
+		$this->api = new MailChimp(Configure::read('Mailchimp.api_key'));
 		if($settings['listId'] == NULL)
 		{
-			$this->listId = Configure::read('Mailchimp.listId');
+			$this->listId = Configure::read('Mailchimp.primary_list');
 		}	
 		else
 			$this->listId = $settings['listId'];
 
 	}
 
-	public function startup($controller) {
+	public function startup(Controller$controller) {
 		$this->controller = $controller;
 	}
 
@@ -55,6 +56,55 @@ class MailchimpComponent extends Component {
 		} else {
 			return true;
 		}
+
+	}
+
+	public function createCampaign($content, $segments, $options, $type = 'regular'){
+
+		$submit = array(
+			'type'=>$type,
+			'options'=>$options,
+			'content'=>$content,
+			'segment_opts'=>$segments
+		);
+//debug($submit); exit();
+		$retval = $this->api->call('/campaigns/create', $submit);
+		return $retval;
+
+
+	}
+	public function sendCampaign($cid){
+
+		$submit = array(
+			'apikey'=>Configure::read('Mailchimp.api_key'),
+			'cid'=>$cid
+		);
+
+		$retval = $this->api->call('/campaigns/send', $submit);
+		return $retval;
+
+
+	}
+	public function getLists(){
+
+		$retval = $this->api->call('lists/list');
+		return $retval;
+
+
+	}
+
+	public function getSegments(){
+		$retval = $this->api->call('lists/segments', array('id'=>Configure::read('Mailchimp.primary_list')));
+		
+		return $retval;
+
+	}
+
+	public function getTemplates(){
+
+		$retval = $this->api->call('templates/list');
+		//$retval = $this->api->templates(array(), null, array('inactives'=>false));
+		return $retval;
 
 	}
 
